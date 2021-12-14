f1a4ac90118c324205679d54dd6c33b4466e720b
Kelvinthedrugger <890225kelvin@gmail.com>
2021-12-15 00:22:22 +0800

    doubly linkedlist worked

--- linkedlist4.c
+++ linkedlist4.c
@@ -2,9 +2,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// checkout malloc in macro
-//#define Malloc(var,size)
-
 #define Malloc(p,n) \
 	if(!((p) = malloc(sizeof(*(p))*(n)))){\
 		fprintf(stderr, "Insufficient memory");\
@@ -27,11 +24,45 @@ Node *newNode(int num){
 	return tnode;
 }
 
-void printlist(Node *head);
-void append(Node *tail, Node *newbie);
+void printlist(Node *head){
+	Node **indirect = &head;
+	while(*indirect){
+		printf("%d ",(*indirect)->data);
+		indirect = &((*indirect)->link);
+	}
+}
 
-void remove_node(Node *head, Node *tail, Node *tar);
+
+void append(Node **tail, Node *newbie){
+	(*tail)->link = newbie;
+	*tail = (*tail)->link;
+}
+
+void remove_node(Node *head, Node *tar, Node **tail){
+	Node **indirect = &head;
+	while((*indirect)->link != tar){
+		indirect = &((*indirect)->link);
+	}
+	if(tar == *tail){
+		*tail = *indirect;
+	}
+	(*indirect)->link = tar->link;
+}
 
 int main(void){
+	
+	Node *root = newNode(1);
+	Node *tail = root;
+	append(&tail,newNode(2));
+	append(&tail,newNode(3));
+	printlist(root);
+	printf("tail: %d\n",tail->data);
+	append(&tail,newNode(4));
+	printlist(root);
+	printf("tail: %d\n",tail->data);
+	remove_node(root,tail,&tail);
+	printlist(root);
+	printf("tail: %d\n",tail->data);
+
 	return 0;
 }

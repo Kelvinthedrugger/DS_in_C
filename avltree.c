ac0dd0bcd52cad9eb7a3d755b0fae9f2bca2a50f
Kelvinthedrugger <890225kelvin@gmail.com>
2021-12-18 19:28:03 +0800

    jdjd

--- avltree.c
+++ avltree.c
@@ -1,9 +1,15 @@
 // AVL Tree (from txb mostly)
-// aka binTree ver of red-black tree ?
+// aka binTree ver of red-black tree
 #include<stdio.h>
 #include<stdlib.h>
 
 #define isFull(ptr) (!(ptr))
+
+#define Malloc(p,n) \
+	if(!((p) = malloc(sizeof(*(p))*(n)))){\
+		fprintf(stderr, "Insufficient memory");\
+		exit(EXIT_FAILURE);\
+	}
 
 typedef struct Node Node;
 struct Node{
@@ -13,20 +19,83 @@ struct Node{
 	Node *rightchild;
 };
 
-int unbalanced = 0;
-Node *root = NULL;
 
 void left_rotation(Node **parent,int *unbalanced){
-	return;
+	Node *child, *grandchild;
+	child = (*parent)->leftchild;
+	if(child->bf == 1){
+		// LL
+		(*parent)->leftchild = child->rightchild;
+		child->rightchild = *parent;
+		(*parent)->bf = 0;
+		(*parent) = child;
+	}
+	else{
+		// LR
+		grandchild = child->rightchild;
+		child->rightchild = grandchild->leftchild;
+		grandchild->leftchild = child;
+		(*parent)->leftchild = grandchild->rightchild;
+		grandchild->rightchild = *parent;
+		switch(grandchild->bf){
+			case 1:
+				(*parent)->bf = -1;
+				child->bf = 0;
+				break;
+			case 0:
+				(*parent)->bf = 0;
+				child->bf = 0;
+				break;
+			case -1:
+				(*parent)->bf = 0;
+				child->bf = 1;
+		}
+		*parent = grandchild;
+	}
+	(*parent)->bf = 0;
+	*unbalanced = 0;
 }
 void right_rotation(Node **parent,int *unbalanced){
-	return;
+	Node *child, *grandchild;
+	child = (*parent)->rightchild;
+	if(child->bf == 1){
+		// RR
+		(*parent)->rightchild = child->leftchild;
+		child->leftchild = *parent;
+		(*parent)->bf = 0;
+		(*parent) = child;
+	}
+	else{
+		// RL
+		grandchild = child->leftchild;
+		child->leftchild = grandchild->rightchild;
+		grandchild->rightchild = child;
+		(*parent)->rightchild = grandchild->leftchild;
+		grandchild->leftchild = *parent;
+		switch(grandchild->bf){
+			case 1:
+				(*parent)->bf = -1;
+				child->bf = 0;
+				break;
+			case 0:
+				(*parent)->bf = 0;
+				child->bf = 0;
+				break;
+			case -1:
+				(*parent)->bf = 0;
+				child->bf = 1;
+		}
+		*parent = grandchild;
+	}
+	(*parent)->bf = 0;
+	*unbalanced = 0;
 }
 
 void avlInsert(Node **parent, int x, int *unbalanced){
 	if(!(*parent)){
 		*unbalanced = 1;
-		*parent = (Node*)malloc(sizeof(Node));
+		//*parent = (Node*)malloc(sizeof(Node));
+		Malloc(parent,1);
 		if(isFull(*parent)){
 			fprintf(stderr,"fff");
 			exit(1);
